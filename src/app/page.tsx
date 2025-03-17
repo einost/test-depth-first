719f1ee599887dbf516d9919fa6bcc23aee0e4f1
Sittinon Non <sittinon.toneon@apar.com>
2025-03-17 19:28:50 +0700

    feat: initial project and add sample login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,120 @@
-import Image from "next/image";
+'use client';
 
-export default function Home() {
+// import classNames from 'classnames';
+import Title from 'antd/es/typography/Title';
+import styles from './page.module.scss';
+import { Button, Col, Form, Input, Row } from 'antd';
+
+interface FormData {
+  name: string;
+  email: string;
+  address?: string;
+  password: string;
+  confirmPassword: string;
+}
+
+const LoginPage = () => {
+  const [form] = Form.useForm<FormData>();
+
+  const _finish = (values: FormData) => {
+    console.log(values);
+  };
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className={styles.container}>
+      <Title color="primary" className={styles.title}>
+        ระบบสมัครสมาชิก
+      </Title>
+      <Form layout="vertical" form={form} onFinish={_finish}>
+        <Row gutter={16}>
+          <Col span={12}>
+            <Form.Item
+              label="ชื่อ-สกุล"
+              name="name"
+              rules={[{ required: true, message: 'กรอกข้อมูลให้ครบถ้วน' }]}>
+              <Input placeholder="ระบุชื่อ-สกุล" />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item
+              label="email"
+              name="email"
+              rules={[
+                { required: true, message: 'กรอกข้อมูลให้ครบถ้วน' },
+                { type: 'email', message: 'อีเมลไม่ถูกต้อง' },
+              ]}>
+              <Input placeholder="ระบุ email" type="email" />
+            </Form.Item>
+          </Col>
+        </Row>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <Row gutter={16}>
+          <Col span={24}>
+            <Form.Item label="ที่อยู่" name="address">
+              <Input.TextArea
+                placeholder="ระบุที่อยู่"
+                rows={1}
+                maxLength={400}
+                showCount
+              />
+            </Form.Item>
+          </Col>
+        </Row>
+
+        <Row gutter={16}>
+          <Col span={12}>
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[
+                { required: true, message: 'กรอกข้อมูลให้ครบถ้วน' },
+                { min: 8, message: 'รหัสผ่านควรมีอย่างน้อย 8 ตัว' },
+              ]}>
+              <Input placeholder="ระบุ Password" type="password" />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item
+              label="Confirm Password"
+              name="confirmPassword"
+              dependencies={['password']}
+              rules={[
+                {
+                  required: true,
+                  message: 'กรอกข้อมูลให้ครบถ้วน',
+                },
+                ({ getFieldValue }) => ({
+                  validator(_, value) {
+                    if (!value || getFieldValue('password') === value) {
+                      return Promise.resolve();
+                    }
+                    return Promise.reject(
+                      new Error('ยืนยันรหัสผ่านไม่ถูกต้อง'),
+                    );
+                  },
+                }),
+              ]}>
+              <Input placeholder="ระบุ Confirm Password" type="password" />
+            </Form.Item>
+          </Col>
+        </Row>
+
+        <Row className="mt-6">
+          <Col span={24}>
+            <Form.Item className="text-center">
+              <Button
+                className="!rounded-3xl min-w-[200px] min-h-[48px]"
+                type="primary"
+                htmlType="submit"
+                size="large">
+                สมัครสมาชิก
+              </Button>
+            </Form.Item>
+          </Col>
+        </Row>
+      </Form>
     </div>
   );
-}
+};
+
+export default LoginPage;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,63 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
+import { PluginAPI } from 'tailwindcss/types/config';
+
+const colors = require('tailwindcss/colors');
+delete colors.lightBlue;
+delete colors.warmGray;
+delete colors.trueGray;
+delete colors.coolGray;
+delete colors.blueGray;
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+    extend: {},
+    colors: {
+      ...colors,
+      primary: {
+        50: '#eaf1f3',
+        100: '#cddfe3',
+        200: '#9ebcc4',
+        300: '#6f98a4',
+        400: '#4b7988',
+        500: '#1c4650',
+        600: '#173941',
+        700: '#122d34',
+        800: '#0d2227',
+        900: '#09181c',
+        950: '#040d0f',
       },
     },
   },
-  plugins: [],
+  plugins: [
+    ({ addBase, theme }: PluginAPI) => {
+      const colors = theme('colors') as Record<
+        string,
+        string | Record<string, string>
+      >;
+      const cssVariables: Record<string, string> = {};
+
+      Object.keys(colors).forEach((key) => {
+        const colorValue = colors[key];
+        if (typeof colorValue === 'object') {
+          Object.keys(colorValue).forEach((shade) => {
+            cssVariables[`--color-${key}-${shade}`] = colorValue[
+              shade
+            ] as string;
+          });
+        } else {
+          cssVariables[`--color-${key}`] = colorValue;
+        }
+      });
+
+      addBase({
+        ':root': cssVariables,
+      });
+    },
+  ],
 };
 export default config;
